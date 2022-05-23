388190f8609732274d9d8f86b4804f060a64636b
arturo <ahpareja@gmail.com>
2022-05-23 17:54:13 -0500

    Add calculator wireframe

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="display">
+          0
+        </div>
+        <div className="buttons line1">
+          <button type="button">AC</button>
+          <button type="button">+/-</button>
+          <button type="button">%</button>
+          <button type="button">/</button>
+        </div>
+        <div className="buttons line2">
+          <button type="button">7</button>
+          <button type="button">8</button>
+          <button type="button">9</button>
+          <button type="button">x</button>
+        </div>
+        <div className="buttons line3">
+          <button type="button">4</button>
+          <button type="button">5</button>
+          <button type="button">6</button>
+          <button type="button">-</button>
+        </div>
+        <div className="buttons line4">
+          <button type="button">1</button>
+          <button type="button">2</button>
+          <button type="button">3</button>
+          <button type="button">+</button>
+        </div>
+        <div className="buttons line5">
+          <button type="button">0</button>
+          <button type="button">.</button>
+          <button type="button">=</button>
+        </div>
+      </div>
     </div>
   );
 }
