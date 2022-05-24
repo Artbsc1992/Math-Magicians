b94a51242cabfdaaf36d9ac4e3f46185764621c1
arturo <ahpareja@gmail.com>
2022-05-23 19:16:16 -0500

    Add calculator wireframe

--- src/App.js
+++ src/App.js
@@ -1,40 +1,9 @@
+import Calculator from './components/Calculator';
+
 function App() {
   return (
     <div className="App">
-      <div className="calculator">
-        <div className="display">
-          0
-        </div>
-        <div className="buttons line1">
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button type="button">/</button>
-        </div>
-        <div className="buttons line2">
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button type="button">x</button>
-        </div>
-        <div className="buttons line3">
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button type="button">-</button>
-        </div>
-        <div className="buttons line4">
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button type="button">+</button>
-        </div>
-        <div className="buttons line5">
-          <button type="button">0</button>
-          <button type="button">.</button>
-          <button type="button">=</button>
-        </div>
-      </div>
+      <Calculator />
     </div>
   );
 }
